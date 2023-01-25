f4f72325da04d03d2a011fe1a206c8949a09324f
lemonilloun <alekssyes4u@gmail.com>
2023-01-25 22:42:18 +0300

    firrst: added main game play func

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -47,8 +47,34 @@ namespace MatchGame
                 string nextEmoji = catsEmoji[index];
                 textBlock.Text = nextEmoji;
                 catsEmoji.RemoveAt(index);
+
+
             }
         }
-            
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+
+        }
     }
 }
